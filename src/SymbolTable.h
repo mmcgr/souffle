64e674f6bc9bd6cc73abd28d486bb64c781425fd
Martin McGrane <mmcgrane@it.usyd.edu.au>
2019-12-27 01:03:10 +1030

    Add triemap of std::string to std::size_t

--- src/SymbolTable.h
+++ src/SymbolTable.h
@@ -20,7 +20,7 @@
 #include "utility/MiscUtil.h"
 #include "utility/ParallelUtil.h"
 #include "utility/StreamUtil.h"
-#include <arrays>
+#include <array>
 #include <atomic>
 #include <cassert>
 #include <deque>
@@ -35,27 +35,31 @@ namespace souffle {
 
 class SymbolStore {
     static constexpr std::size_t BLOCK_SIZE = 1024 * 1024;
-    std::array<std::atomic<string*>>, BLOCK_SIZE > symbols{};
+    std::array<std::atomic<std::string*>, BLOCK_SIZE> symbols{};
     std::atomic<std::size_t> currentSize{0};
+
+    void insert(std::size_t id, std::string symbol) {
+        assert(id <= currentSize && "Index out of bounds");
+        symbols[id % BLOCK_SIZE][id / BLOCK_SIZE] = symbol;
+    }
 
 public:
     std::size_t getNextId() {
         std::size_t result = currentSize++;
         while (symbols[result % BLOCK_SIZE] == nullptr) {
-            std::string* newArray = new std::string[BLOCK_SIZE];
-            for (std::size_t& current : newArray) {
-                current = nullptr;
+            std::string* oldArray = symbols[result % BLOCK_SIZE];
+            if (oldArray != nullptr) {
+                break;
             }
-            if (!symbols[result % BLOCK_SIZE].compare_exchange_strong(nullptr, newArray)) {
+            std::string* newArray = new std::string[BLOCK_SIZE];
+            for (std::size_t i = 0; i < BLOCK_SIZE; ++i) {
+                newArray[i] = nullptr;
+            }
+            if (!symbols[result % BLOCK_SIZE].compare_exchange_strong(oldArray, newArray)) {
                 delete newArray;
             }
         }
         return result;
-    }
-
-    void insert(std::size_t id, std::string symbol) {
-        assert(id <= currentSize, "Index out of bounds");
-        symbols[id % BLOCK_SIZE][id / BLOCK_SIZE] = new std::string(symbol);
     }
 
     std::size_t insert(std::string symbol) {
@@ -65,7 +69,7 @@ public:
     }
 
     const std::string& getSymbol(std::size_t id) const {
-        assert(id <= currentSize, "Index out of bounds");
+        assert(id <= currentSize && "Index out of bounds");
         return symbols[id % BLOCK_SIZE][id / BLOCK_SIZE];
     }
 
@@ -73,18 +77,137 @@ public:
     SymbolStore(const SymbolStore&) = delete;
     ~SymbolStore() {
         for (std::atomic<std::string*>& current : symbols) {
-            for (std::size_t i = 0; i < BLOCK_SIZE; ++i) {
-                delete current[i];
-            }
+            delete current.load();
         }
     }
-}
+};
 
 // Concurrent trie map, tips store ids
 // Get id by inserting string into SymbolStore
 // Add string to id mapping to idstore
+//
+// Hmm. TODO: mark deleted nodes? Currently we risk reusing a deleted parent
 class IdStore {
-}
+public:
+    struct IdStoreNode {
+        std::atomic<std::size_t> id = 0;
+        std::atomic<std::unordered_map<char, std::atomic<IdStoreNode*>>*> children =
+                new std::unordered_map<char, std::atomic<IdStoreNode*>>;
+        IdStoreNode* get(char c) {
+            auto it = children.load()->find(c);
+            if (it == children.load()->end()) {
+                return nullptr;
+            }
+            return it->second;
+        }
+        IdStoreNode() = default;
+        IdStoreNode(std::size_t id, std::unordered_map<char, std::atomic<IdStoreNode*>>* children)
+                : id(id), children(children) {}
+        bool addChild(char c, IdStoreNode* node) {
+            auto* oldChildren = children.load();
+            if (oldChildren->count(c) != 0) {
+                return false;
+            }
+            auto* newChildren = new std::unordered_map<char, std::atomic<IdStoreNode*>>();
+            for (auto& pair : *oldChildren) {
+                (*newChildren)[pair.first].store(pair.second);
+            }
+            (*newChildren)[c].store(node);
+            if (children.compare_exchange_strong(oldChildren, newChildren)) {
+                delete oldChildren;
+                return true;
+            } else {
+                delete newChildren;
+                return false;
+            }
+        }
+        ~IdStoreNode() {
+            for (auto& child : *children) {
+                delete child.second;
+            }
+            delete children.load();
+        }
+    };
+    std::atomic<IdStoreNode*> root = new IdStoreNode(0, {});
+
+    IdStoreNode* get(const std::string& symbol) {
+        IdStoreNode* current = root;
+        for (char c : symbol) {
+            current = current->get(c);
+            if (current == nullptr) {
+                break;
+            }
+        }
+        return current;
+    }
+    std::size_t getId(const std::string& symbol) {
+        IdStoreNode* node = get(symbol);
+        if (node == nullptr) {
+            throw std::invalid_argument("Symbol not found");
+        }
+        return node->id;
+    }
+
+    bool contains(const std::string& symbol) {
+        return get(symbol) != nullptr;
+    }
+    std::size_t insert(std::size_t id, const std::string& symbol) {
+        if (symbol.empty()) {
+            return 0;
+        }
+        // Check root node
+        while (root.load()->get(symbol.front()) == nullptr) {
+            IdStoreNode* newChild = makeChain(id, symbol, 0);
+            if (root.load()->addChild(symbol.front(), newChild)) {
+                return id;
+            }
+            delete newChild;
+        }
+        // Search for matching node
+        IdStoreNode* parent = root;
+        IdStoreNode* current = parent->get(symbol.front());
+        IdStoreNode* next = nullptr;
+        std::size_t i = 1;
+        for (; i < symbol.size();) {
+            char c = symbol[i];
+            next = current->get(c);
+            if (next != nullptr) {
+                parent = current;
+                current = next;
+                ++i;
+                continue;
+            }
+            IdStoreNode* newChild = makeChain(id, symbol, i);
+            if (current->addChild(symbol[i], newChild)) {
+                return id;
+            } else {
+                delete newChild;
+            }
+        }
+        // A matching node is found - either it's a leaf and we return the existing id
+        // Or it's not and we insert the id
+        if ((*current->children).empty()) {
+            return current->id;
+        }
+        auto cId = current->id.load();
+        // Found a matching node. Now we either retur the id or set it
+        if (cId > 0) {
+            return cId;
+        }
+        // If the id is 0 update it
+        current->id.compare_exchange_strong(cId, id);
+        return current->id;
+    }
+    IdStoreNode* makeChain(std::size_t id, std::string symbol, std::size_t index) {
+        IdStoreNode* root = new IdStoreNode();
+        if (index < symbol.size()) {
+            (*root->children)[symbol[index]].store(makeChain(id, symbol, index + 1));
+        } else {
+            root->id = id;
+        }
+        return root;
+    }
+};
 
 /**
  * @class SymbolTable
@@ -104,8 +227,8 @@ private:
     /** Map strings to indices. */
     std::unordered_map<std::string, size_t> strToNum;
 
-    /** Convenience method to place a new symbol in the table, if it does not exist, and return the index of
-     * it. */
+    /** Convenience method to place a new symbol in the table, if it does not exist, and return the index
+     * of it. */
     inline size_t newSymbolOfIndex(const std::string& symbol) {
         size_t index;
         auto it = strToNum.find(symbol);

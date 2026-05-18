d1292a4d26c438b5bc65e40c660720e174abc650
Martin McGrane <mmcgr@users.noreply.github.com>
2026-05-18 12:08:43 +1000

    Snip pre-20 deduction guide

--- src/parser/ParserUtils.cpp
+++ src/parser/ParserUtils.cpp
@@ -248,9 +248,6 @@ template <class... Ts>
 struct overloaded : Ts... {
     using Ts::operator()...;
 };
-// explicit deduction guide (not needed as of C++20)
-template <class... Ts>
-overloaded(Ts...) -> overloaded<Ts...>;
 
 struct Cursor {
     explicit Cursor(const ast::TokenStream& ts) : stream(ts), index(0) {}

19fe35dab98ab3cdfb52342c894d0feeaa7ea658
Martin McGrane <mmcgr@users.noreply.github.com>
2026-05-27 13:52:50 +1000

    Use std::bit_cast for bit casting

--- src/include/souffle/RamTypes.h
+++ src/include/souffle/RamTypes.h
@@ -17,9 +17,9 @@
 #pragma once
 
 #include <array>
+#include <bit>
 #include <cstdint>
 #include <cstring>
-#include <iostream>
 #include <limits>
 #include <type_traits>
 
@@ -66,11 +66,6 @@ template <typename T>
 constexpr bool isRamType = (std::is_same<T, RamDomain>::value || std::is_same<T, RamSigned>::value ||
                             std::is_same<T, RamUnsigned>::value || std::is_same<T, RamFloat>::value);
 
-/**
-In C++20 there will be a new way to cast between types by reinterpreting bits (std::bit_cast),
-but as of January 2020 it is not yet supported.
-**/
-
 /** Cast a type by reinterpreting its bits. Domain is restricted to Ram Types only.
  * Template takes two types (second type is never necessary because it can be deduced from the argument)
  * The following always holds
@@ -81,9 +76,7 @@ template <typename To = RamDomain, typename From>
 To ramBitCast(From source) {
     static_assert(isRamType<From> && isRamType<To>, "Bit casting should only be used on Ram Types.");
     static_assert(sizeof(To) == sizeof(From), "Can't bit cast types with different size.");
-    To destination;
-    memcpy(&destination, &source, sizeof(destination));
-    return destination;
+    return std::bit_cast<To>(source);
 }
 
 /** lower and upper boundaries for the ram types **/
